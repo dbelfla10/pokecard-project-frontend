84e1448c5efc141cdd96d68462875dafb3bf4549
Diego <db_elfla10@outlook.es>
2025-01-02 21:31:29 -0500

    Set up file structure, add fonts and remove unnecessary code

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="page">
+      <h1 className="title">Holaaaaa</h1>
+      <p className="text">Como ezta</p>
+    </div>
+  );
+}
+
+export default App;
